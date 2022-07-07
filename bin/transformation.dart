466cae7e12c389498d0ce0a7fa25b0dd8d52a63a
vishnuacappella <acappella.studiov@gmail.com>
2022-07-07 12:26:18 +0530

    dart created

---
+++ bin/transformation.dart
@@ -0,0 +1,5 @@
+import 'package:transformation/transformation.dart' as transformation;
+
+void main(List<String> arguments) {
+  print('Hello world: ${transformation.calculate()}!');
+}
---
+++ test/transformation_test.dart
@@ -0,0 +1,8 @@
+import 'package:transformation/transformation.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
